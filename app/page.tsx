a852b8a3f688871f856c57e8b9a067b9f1f51469
DUPIN <maximedupin1992@gmail.com>
2023-07-28 12:40:59 +0200

    finish

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-[#396853] to-[#18447e] p-10 flex flex-col justify-center items-center">
+    <div className="overflow-hidden min-h-screen bg-gradient-to-br from-[#396853] to-[#18447e] p-10 pt-6 flex flex-col justify-center items-center">
       <Card className="max-w-4xl mx-auto">
         <Image
           src="/ivern.jpg"
@@ -15,11 +15,11 @@ export default function Home() {
           height={200}
           className="rounded-full mx-auto"
         />
-        <Text className="text-6xl font-bold text-center my-10"> AIvern</Text>
+        <Text className="text-5xl font-bold text-center my-8"> AIvern</Text>
         <Subtitle className="text-xl text-center">
           {" "}
-          Hoho, I &apos;m AIvern, the Green Father. I&apos; m here to help you Tell me where
-          you live !
+          Hoho, I &apos;m AIvern, the Green Father. I&apos; m here to help you
+          Tell me where you live !
         </Subtitle>
 
         <Divider className="my-10" />
